41b9e48a69afcb5666dcab37f25bcb0c216bdbfa
nigdanil <91422783+nigdanil@users.noreply.github.com>
2023-02-01 11:44:37 +0300

    Fix range

--- scripts/work_with_book/regexPageBookPages.py
+++ scripts/work_with_book/regexPageBookPages.py
@@ -1,35 +1,19 @@
 import re
 
 from document import Document as readDoc
-from readPage import ReadPage
 
 
 class RegexBookPages:
 
     doc = readDoc()
 
-    # What we Have
-    # https://libcat.ru/knigi/fantastika-i-fjentezi/alternativnaya-istoriya/385869-svetlana-shepot-galkino-schaste.html
-
-    # Using the regex for string to find this substrings (385869-1)
-
-    # What we need
-    # https://libcat.ru/knigi/fantastika-i-fjentezi/alternativnaya-istoriya/385869-1-svetlana-shepot-galkino-schaste.html
-
     loadDoc = doc.load_document(r'data\oneBookLinks.txt')
-
-    # Read page count
-    # readPage = ReadPage()
-    # print(readPage.read_content(soup))
 
     # Need create func what get data array
     # Mocking data array
-    temp = [1, 2, 3, 4, 5, 6, 7, 100]
-    res = [temp[0], temp[-1]]
+    temp = [1, 2, 3, 4, 5, 6, 7, 272]
 
-    print(res)
-
-    for i in range(res[0], res[1]):
+    for i in range(temp[0], (temp[-1]+temp[0])):
 
         string_first_part = re.findall(r'^https.*/[0-9]', loadDoc)
 
