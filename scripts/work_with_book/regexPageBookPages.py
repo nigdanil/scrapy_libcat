bb94da73704af7bef81d3cc6401a88bead737264
nigdanil <91422783+nigdanil@users.noreply.github.com>
2023-02-01 11:54:10 +0300

    Fix regex and test them

--- scripts/work_with_book/regexPageBookPages.py
+++ scripts/work_with_book/regexPageBookPages.py
@@ -5,24 +5,36 @@ from document import Document as readDoc
 
 class RegexBookPages:
 
-    doc = readDoc()
+    def __init__(self):
+        self.temp = []
 
-    loadDoc = doc.load_document(r'data\oneBookLinks.txt')
+    def build_book_links(self, arg):
 
-    # Need create func what get data array
-    # Mocking data array
-    temp = [1, 2, 3, 4, 5, 6, 7, 272]
+        doc = readDoc()
 
-    for i in range(temp[0], (temp[-1]+temp[0])):
+        loadDoc = doc.load_document(r'data\oneBookLinks.txt')
 
-        string_first_part = re.findall(r'^https.*/[0-9]', loadDoc)
+        # Need create func what get data array
+        # Mocking data array
+        # temp = [1, 2, 3, 4, 5, 6, 7, 272]
 
-        string_last_part = re.findall(r'\/[0-9].*\.html$', loadDoc)
+        for i in range(arg[0], (arg[-1]+arg[0])):
 
-        a = re.sub(r'^\/[0-9]*-', '', string_last_part[0])
+            string_first_part = re.findall(r'^https.*/[0-9]', loadDoc)
 
-        b = re.findall(r'^\/[0-9]*[-]{1}', string_last_part[0])
+            string_last_part = re.findall(r'\/[0-9].*\.html$', loadDoc)
 
-        c = (b[0] + (f'{i}-'))
-        # Build the links
-        print(f'{string_first_part[0][:-2]}{c}{a}')
+            a = re.sub(r'^\/[0-9]*-', '', string_last_part[0])
+
+            b = re.findall(r'^\/[0-9]*[-]{1}', string_last_part[0])
+
+            c = (b[0] + (f'{i}-'))
+            # Build the book pages links
+            print(f'{string_first_part[0][:-2]}{c}{a}')
+            self.temp.append(f'{string_first_part[0][:-2]}{c}{a}')
+
+        return self.temp
+
+
+foo = RegexBookPages()
+foo.build_book_links([1, 2, 3, 4, 5])
